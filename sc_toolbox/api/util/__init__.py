aa127b8bb25335495a0bf73bca83749c4b5c3990
zethson <lukas.heumos@posteo.net>
2021-04-29 11:26:00 +0200

    add memory efficient concatenating
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- sc_toolbox/api/util/__init__.py
+++ sc_toolbox/api/util/__init__.py
@@ -1,3 +1,8 @@
+from typing import List
+
+from anndata import AnnData
+
+
 def timestamp():
     """
     Custom timestamp in common EU format.
@@ -29,3 +34,30 @@ def binarize_score(adata, score_label: str, threshold: float):
     result = ["positive" if score > threshold else "negative" for score in adata.obs[score_label]]
 
     return result
+
+
+def read_concatenate_to_adata(file_paths: List[str]) -> AnnData:
+    """
+    Parses a list of file paths and concatenates them memory efficiently into a single AnnData object.
+
+    Args:
+        file_paths: List of file paths
+
+    Returns:
+        Single AnnData object containing all concatenated files
+    """
+    import anndata as ad
+    import gc
+    import scipy.sparse
+
+    for index, file_name in enumerate(file_paths):
+        if index == 1:
+            adata_concatenated = ad.read(file_name)
+            adata_concatenated.X = scipy.sparse.csr_matrix(adata_concatenated.X)
+        else:
+            adata = ad.read(file_name)
+            adata.X = scipy.sparse.csr_matrix(adata.X)
+            gc.collect()
+            adata_concatenated = adata_concatenated.concatenate(adata)
+
+    return adata_concatenated
