6c3190ab9a1d741e8c8b1fbe4a1bbdf5225b3bdf
zethson <lukas.heumos@posteo.net>
2021-05-08 22:11:39 +0200

    Bump version from 0.5.0-SNAPSHOT to 0.5.0

--- sc_toolbox/__init__.py
+++ sc_toolbox/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Lukas Heumos"
 __email__ = "lukas.heumos@posteo.net"
-__version__ = "0.5.0-SNAPSHOT"
+__version__ = "0.5.0"
 
 from sc_toolbox.api import plot
 from sc_toolbox.api import calc
